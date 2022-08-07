59a8868fea0e3d849e604fca6bc5b9e9c107d811
Tim <timrus99@gmail.com>
2022-08-07 11:47:53 +0200

    Update on welcome screen

--- app/src/main/java/com/example/farmlog/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/farmlog/welcome/WelcomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.farmlog.welcome
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import com.example.farmlog.R
@@ -22,6 +23,21 @@ class WelcomeActivity : AppCompatActivity() {
             val intent = Intent(this, LandsMapActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            val decorView: View = window.decorView
+            decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            )
         }
     }
 
